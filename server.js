a31cf6c4589fac32651f5cdcf6f792892dbc9413
Roger Zavala <rogeliozavala15@gmail.com>
2020-07-22 21:00:54 -0700

    finished refactoring for ssr

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const router = require('./routes/router')
 const exphbs = require('express-handlebars');
-
+const path = require('path')
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+//Default File is public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Handlebars
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-app.listen(PORT, () => console.log(`app listening on http://localhost:${3000}`));
+//Routes
+app.use('/', router)
+
+
+app.listen(PORT, () => console.log(`app listening on http://localhost:${PORT}`));
