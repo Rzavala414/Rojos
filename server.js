bae39eddae9ed1eb2681d80acb33162e999b31a4
Roger Zavala <rogeliozavala15@gmail.com>
2020-07-22 20:08:43 -0700

    refactored index.html to handlbars template

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 app.listen(PORT, () => console.log(`app listening on http://localhost:${3000}`));
