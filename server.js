2f5fe1c468dd7dfe588a571ddaf5170276fa9f8d
Roger Zavala <rogeliozavala15@gmail.com>
2020-07-22 21:08:17 -0700

    added dotenv pathway

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const router = require('./routes/router')
+const router = require('./routes/router');
+const dotenv = require('dotenv')
 const exphbs = require('express-handlebars');
 const path = require('path')
 const app = express();
