4f9c6ab4bb55fce720c8a5f17af97cdb9c98d81a
Roger Zavala <rogeliozavala15@gmail.com>
2020-07-23 21:27:37 -0700

    established connection to database

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const router = require('./routes/router');
-const dotenv = require('dotenv')
 const exphbs = require('express-handlebars');
+const connectDB = require('./config/database');
 const path = require('path')
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+//Enable dotenv to config file
+require('dotenv').config({ path: './config/keys.env' });
+
+//Connect Database
+connectDB();
+
 //Default File is public
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Express Handlebars
 app.engine('.hbs', exphbs({extname: '.hbs'}));
