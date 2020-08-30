d8c638dbf0d1df242e219071175a5e6d67c3397f
Roger Zavala <rogeliozavala15@gmail.com>
2020-08-30 10:20:35 -0700

    cleaned up comments

--- routes/events.js
+++ routes/events.js
@@ -22,9 +22,7 @@ eventsRouter.post('/',  async(req, res, next) =>{
     }
     
 });
-//jfieidjdfj
-//jfijdfij
-//jfldjflkjd    
+  
 // Displays the Edit event page with item to update
 eventsRouter.get('/edit/:id', (req, res, next) =>{
     res.render('events/edit')
