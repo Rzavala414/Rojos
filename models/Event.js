bc6ccf438da5385ddcfa0a379e9144c4ae8dbffa
Roger Zavala <rogeliozavala15@gmail.com>
2020-09-22 15:38:11 -0700

    fixed parsing issue with account infor

--- models/Event.js
+++ models/Event.js
@@ -12,7 +12,7 @@ const EventSchema = new mongoose.Schema({
     },
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+        ref: 'user'
     }
 });
 
--- server.js
+++ server.js
@@ -44,6 +44,7 @@ app.set('view engine', '.hbs');
 //Body parser
 app.use(express.urlencoded({ extended: false}))
 app.use(express.json())
+
 app.use(methodOverride(function (req, res) {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
       // look in urlencoded POST bodies and delete it
